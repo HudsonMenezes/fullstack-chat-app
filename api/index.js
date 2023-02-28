52f318bac9e8237db156f77fde437175986598cf
Hudson Menezes <hudson.bm23@gmail.com>
2023-02-28 15:20:18 -0300

    creating userSchema

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", (req, res) => {});
+
+app.listen(8000);
+
+//
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,18 +1,23 @@
-import useState from "react";
+import { useState } from "react";
 
 export default function Register() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto">
+      <form className="w-64 mx-auto mb-12">
         <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           type="text"
           placeholder="username"
           className="block w-full rounded-sm p-2 mb-2 border"
         />
         <input
-          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
           placeholder="password"
           className="block w-full rounded-sm p-2 mb-2 border"
         />
