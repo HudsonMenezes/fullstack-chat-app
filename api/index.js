0f53fe2c3396c364b989ea2a3f8cf463c4287b18
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-08 11:00:28 -0300

    killing old connections

--- api/index.js
+++ api/index.js
@@ -126,6 +126,35 @@ const server = app.listen(4040);
 const wss = new ws.WebSocketServer({ server });
 
 wss.on("connection", (connection, req) => {
+  function notifyAboutOnlinePeople() {
+    [...wss.clients].forEach((client) => {
+      client.send(
+        JSON.stringify({
+          online: [...wss.clients].map((c) => ({
+            userId: c.userId,
+            username: c.username,
+          })),
+        })
+      );
+    });
+  }
+
+  connection.isAlive = true;
+
+  connection.timer = setInterval(() => {
+    connection.ping();
+    connection.deathTimer = setTimeout(() => {
+      connection.isAlive = false;
+      connection.terminate();
+      notifyAboutOnlinePeople();
+      console.log("dead");
+    }, 1000);
+  }, 5000);
+
+  connection.on("pong", () => {
+    clearTimeout(connection.deathTimer);
+  });
+
   // read username and id from the cookie for this connection
   const cookies = req.headers.cookie;
   if (cookies) {
@@ -170,14 +199,5 @@ wss.on("connection", (connection, req) => {
   });
 
   // notify everyone about online people (when someone connects)
-  [...wss.clients].forEach((client) => {
-    client.send(
-      JSON.stringify({
-        online: [...wss.clients].map((c) => ({
-          userId: c.userId,
-          username: c.username,
-        })),
-      })
-    );
-  });
+  notifyAboutOnlinePeople();
 });
