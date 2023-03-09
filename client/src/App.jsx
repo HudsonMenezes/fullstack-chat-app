b50b171837ea21b991c68f04c6f54712128244dc
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-09 16:28:39 -0300

    api link update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://fullstack-chat-app-api.vercel.app/";
   axios.defaults.withCredentials = true;
 
   return (
