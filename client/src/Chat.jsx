132a403718575df131062c26a552d48d01a76a1c
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-09 18:31:55 -0300

    ws update

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -21,7 +21,7 @@ export default function Chat() {
   }, []);
 
   function connectToWs() {
-    const ws = new WebSocket("ws://fullstack-chat-app-api.vercel.app/");
+    const ws = new WebSocket("wss://fullstack-chat-app-api.vercel.app/");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
