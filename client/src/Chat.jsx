ee5f29a9080b8612764b97dc3c3cb0affd41e838
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-09 18:29:53 -0300

    changing ws server

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -21,7 +21,7 @@ export default function Chat() {
   }, []);
 
   function connectToWs() {
-    const ws = new WebSocket("ws://localhost:4040");
+    const ws = new WebSocket("ws://fullstack-chat-app-api.vercel.app/");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
