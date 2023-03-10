49ff7c61e21ab362744c825b0111801514f9bfff
Hudson Menezes <hudson.bm23@gmail.com>
2023-03-10 12:13:00 -0300

    again

--- api/index.js
+++ api/index.js
@@ -60,6 +60,16 @@ async function getuserDataFromRequest(req) {
   });
 }
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
